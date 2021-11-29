de5707dc5035e2a003022e48113a15a8f8852975
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2021-11-29 21:53:26 +0700

    Mount and Unmounted

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import { useState } from 'react'
+import Content from './Content'
 
 function App() {
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(() => {
-    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
-    console.log(storageJobs)
-    return storageJobs ?? []
-  })
-
-  const handleSubmit = () => {
-    setJobs(prevState => {
-      const newJobs = [...prevState, job]
-      localStorage.setItem('jobs', JSON.stringify(newJobs))
-      return newJobs
-    })
-    setJob('')
-  }
+  const [show, setShow] = useState(false)
 
   return (
     <div className="App" style={{ padding: 32 }}>
-      <input
-        value={job}
-        onChange={e => setJob(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,7 @@
+function Content() {
+  return (
+    <h1>Hi anh em</h1>
+  )
+}
+
+export default Content
