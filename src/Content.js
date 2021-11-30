0f7d5f18ed64426d4dee530fd92c255c2dcca92d
Anh Phạm Quang <anh.pq@teko.vn>
2021-11-30 14:48:26 +0700

    useEffect with dependencies

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,32 @@
 import { useEffect, useState } from 'react'
 
 function Content() {
+  const tabs = ['posts', 'comments', 'albums']
+
   const [title, setTitle] = useState('')
   const [posts, setPosts] = useState([])
+  const [type, setType] = useState('posts')
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    console.log('type changed')
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then(res => res.json())
       .then(posts => {
         setPosts(posts)
       })
-  }, [])
+  }, [type])
 
   return (
     <div>
+      {tabs.map(tab => (
+        <button 
+          key={tab}
+          style={type === tab ? {color: '#fff', backgroundColor: '#333'} : {}}
+          onClick={() => setType(tab)}
+        >
+            {tab}
+        </button>
+      ))}
       <input
         value={title}
         type="text"
@@ -21,7 +34,7 @@ function Content() {
       />
       <ul>
         {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.title ?? post.name}</li>
         ))}
       </ul>
     </div>
