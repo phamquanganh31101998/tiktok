f4ba18047d6bbdb03619da436a1ca4858238d095
Anh Phạm Quang <anh.pq@teko.vn>
2021-12-01 15:10:03 +0700

    useEffect with fake chat app

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,53 @@
 import { useEffect, useState } from 'react'
 
+const lessons = [
+  {
+    id: 1,
+    name: 'Learning JS'
+  },
+  {
+    id: 2,
+    name: 'Learning React'
+  },
+  {
+    id: 3,
+    name: 'Learning Vue'
+  },
+]
+
 function Content() {
-  const [avatar, setAvatar] = useState()
+  const [lessonId, setLessonId] = useState(1)
 
   useEffect(() => {
-    return () => {
-      avatar && URL.revokeObjectURL(avatar.preview)
+    const handleComment = ({ detail }) => {
+      console.log(detail)
     }
-  }, [avatar])
+    window.addEventListener(`lesson-${lessonId}`, handleComment)
 
-  const handlePreviewAvatar = (e) => {
-    const file = e.target.files[0]
-    
-    file.preview = URL.createObjectURL(file)
-    
-    setAvatar(file)
-  }
+    return () => {
+      window.removeEventListener(`lesson-${lessonId}`, handleComment)
+    }
+  }, [lessonId])
 
   return (
     <div>
-      <input
-        type="file"
-        onChange={handlePreviewAvatar}
-      />
-      {avatar && (
-        <img src={avatar.preview} alt="" width="80%"/>
-      )}
+      <ul>
+        {
+          lessons.map(lesson => (
+            <li
+              key={lesson.id}
+              style={
+                {
+                  color: lessonId === lesson.id ? 'red' : '#333'
+                }
+              }
+              onClick={() => setLessonId(lesson.id)}
+            >
+              {lesson.name}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
