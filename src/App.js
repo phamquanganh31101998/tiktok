31418dac0536c4891d2b413080296ba3dc38e1f6
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2021-12-02 22:22:54 +0700

    memo HOC

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import { useState, useRef, useEffect } from 'react'
-
+import { useState } from 'react'
+import Content from './Content'
 function App() {
-  const [count, setCount] = useState(60)
-  let timerId = useRef()
-  const prevCount = useRef()
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    prevCount.current = count
-  }, [count])
-
-  const handleStart = () => {
-    timerId.current = setInterval(() => {
-      setCount(prevCount => prevCount - 1)
-    }, 1000)
+  const increase = () => {
+    setCount(count + 1)
   }
-
-  const handleStop = () => {
-    clearInterval(timerId.current)
-  }
-
-  const h1Ref = useRef()
-
-  useEffect(() => {
-    const rect = h1Ref.current.getBoundingClientRect()
-
-    console.log(rect)
-  })
 
   return (
     <div className="App" style={{ padding: 32 }}>
-      <h1 ref={h1Ref}>{count}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <Content />
+      <h1>{count}</h1>
+      <button onClick={increase}>Click me!</button>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,11 @@
-import { useState, useLayoutEffect } from 'react'
+import { memo } from 'react'
 
 function Content() {
-  const [count, setCount] = useState(0)
-
-  useLayoutEffect(() => {
-    if (count > 3) {
-      setCount(0)
-    }
-  }, [count])
-
-  const handleRun = () => {
-    setCount(count + 1)
-  }
-
-
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={handleRun}>Run</button>
+      <h2>Hello anh em</h2>
     </div>
   )
 }
 
-export default Content
+export default memo(Content)
