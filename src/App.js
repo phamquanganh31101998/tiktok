dabd0ec1abd9217bb5d7f073116b7a5c3c19cf20
Anh Phạm Quang <anh.pq@teko.vn>
2021-12-09 16:02:47 +0700

    clsx library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import Heading from "./components/Heading"
-import Paragraph from "./components/Paragraph"
+import Button from "./components/Button"
 import GlobalStyles from "./components/GlobalStyles"
 
 function App() {
   return (
     <GlobalStyles>
       <div style={{ padding: 20 }}>
-        <Heading/>
-        <Paragraph/>
-
-        <div className="d-flex">
-          <div>Item 1</div>
-          <div>Item 2</div>
-        </div>
+        <Button/>
+        <Button primary/>
       </div>
     </GlobalStyles>
   )
